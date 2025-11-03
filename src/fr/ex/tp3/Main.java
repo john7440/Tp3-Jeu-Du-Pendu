5a193f3de77dc90c797046152ddeb19af3290270
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-11-03 13:32:54 +0100

    ajout d'un replaceAll pour cacher le mot a l'utilisateur

--- src/fr/ex/tp3/Main.java
+++ src/fr/ex/tp3/Main.java
@@ -1,10 +1,23 @@
 package fr.ex.tp3;
+import java.util.Random;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
 		String[] wordsList = {"cookie", "chocolat", "patisserie", "anticonstitutionnelement", "sandwich", "marteau", "capitalisme", "alphabet", "confiture"};
+		
+		// Randomization
+		Random random = new Random();
+		int rdmIndex = random.nextInt(wordsList.length);
+		String randomizedWord = wordsList[rdmIndex];
+		
+		System.out.println(randomizedWord);
+		
+		// Modifications
+		String hidedWord = randomizedWord.replaceAll(".", "_ ");
+		
+		System.out.println("Mot Mystère: " + hidedWord);
 
 	}
 
