2508e26608b0c3bf6bbda47ae79f507d93a8f48b
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-11-03 14:51:19 +0100

    ajout d'une condition de victoire + updating du mot a trouvé

--- src/fr/ex/tp3/Main.java
+++ src/fr/ex/tp3/Main.java
@@ -4,13 +4,6 @@ import java.util.Scanner;
 
 public class Main {
 	
-	public static String userInput() {
-		
-		Scanner scan = new Scanner(System.in);
-		String usrChoice  = scan.next();
-		scan.close();
-		return usrChoice;
-	}
 	
 	public static String randomizer(String[] wordList) {
 		
@@ -21,27 +14,70 @@ public class Main {
 		
 	}
 	
-	public static String hidingWordFromUser(String word) {
+	public static char [] hidingWordFromUser(String word) {
 		
-		// Modifications
-		String hidedWord = word.replaceAll(".", "_ ");
-		return hidedWord;
+		char[] hiddenWord = new char[word.length()];
+        for (int i = 0; i < word.length(); i++) {
+            hiddenWord[i] = '_';
+        }
+        return hiddenWord;
+	}
+	
+	public static void displayHidenWord(char[] hidden) {
+		
+		// Affichage
+		System.out.println("Mot mystère : ");
+		for (char c: hidden) {
+			System.out.print(c + " ");
+		}
+		System.out.println();
+	}
+	
+	public static boolean updatingHiddenWord(String word, char[]hidden, char userInput) {
+		
+		// verification
+		boolean found = false;
+		for (int i =0; i < word.length(); i++) {
+			if (word.charAt(i) == userInput) {
+				hidden[i] = userInput;
+				found = true;
+			}
+		}
+		return found;
+	}
+	
+	public static boolean winCondition(char[] hidden) {
+		for (char c : hidden) {
+			if (c == '_') return false;
+		}
+		return true;
 	}
 
 	public static void main(String[] args) {
 		
+		// Our data
 		String[] wordsList = {"cookie", "chocolat", "patisserie", "anticonstitutionnelement", "sandwich", "marteau", "capitalisme", "alphabet", "confiture"};
 		
-		
 		String randomizedWord = randomizer(wordsList);
+		char[] hidedWord = hidingWordFromUser(randomizedWord);
 		
+		// Scanner
+		Scanner scan = new Scanner(System.in);
 		
-		
-		
-		System.out.println("Mot Mystère: " + hidingWordFromUser(randomizedWord));
-		
-		System.out.println("Veuilez entrer une lettre: ");
-		String userChoice = userInput();
+		while (!winCondition(hidedWord)) {
+			displayHidenWord(hidedWord);
+			System.out.println("Entrez une lettre: ");
+			char usrChoice = scan.next().toLowerCase().charAt(0);
+			
+			if (updatingHiddenWord(randomizedWord, hidedWord, usrChoice)) {
+				System.out.println("Bien joué !");
+            } else {
+                System.out.println("Raté !");
+            }
+					
+		}
+		System.out.println("Bravo ! Le mot était : " + randomizedWord);
+        scan.close();
 		
 			
 		}
