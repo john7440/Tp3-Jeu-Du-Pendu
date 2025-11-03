8de217dc3d2fa252da57a964a01ca75208ee49f4
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-11-03 13:14:59 +0100

    création de la liste de mot + import et initialisation de Random

--- src/fr/ex/tp3/Main.java
+++ src/fr/ex/tp3/Main.java
@@ -3,7 +3,8 @@ package fr.ex.tp3;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String[] wordsList = {"cookie", "chocolat", "patisserie", "anticonstitutionnelement", "sandwich", "marteau", "capitalisme", "alphabet", "confiture"};
 
 	}
 
